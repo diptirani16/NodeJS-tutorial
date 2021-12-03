178673fc33d51f722fd103d587e060ee0e88201a
Dipti Rani <gdipti642@gmail.com>
2021-12-03 18:57:44 +0530

    http routing in nodejs

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,7 +1,20 @@
 const http  = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the other side!')
+    // console.log(req.url);
+    if(req.url == '/') {
+        res.end('Hello from the other side!')
+    }
+    else if(req.url == '/about') {
+        res.end('About page!')
+    }
+    else if(req.url == '/contact') {
+        res.end('Contact page!')
+    } 
+    else {
+        res.writeHead(404, { "Content-type": "text/html"});
+        res.end('<h1> 404 ERROR! PAGE NOT FOUND! </h1>')
+    }
 });
 
 server.listen(8000, 'localhost', () => {
