99853b40e46197361cf44d1c49ced65280086bbb
Dipti Rani <gdipti642@gmail.com>
2021-12-03 18:33:44 +0530

    create your own web server

---
+++ modWrapperFunction/index.js
@@ -0,0 +1,7 @@
+(function (exports, require, module, _filename, _dirname) {
+    const name = "Dipti";
+    console.log(name);
+    console.log(_filename);
+    console.log(_dirname);
+})();
+// console.log(name)
